3b4ff057d30d4a10b588d121b83944aaa6516d68
Michael ugenlo <michaelugenlo16@gmail.com>
2023-02-16 15:49:58 -0800

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main -A program that prints size of various types
- * Return 0
+ *
+ * Return:0
  */
+
 int main(void)
 {
 char number;
@@ -15,6 +17,5 @@ printf("\nSize of an int: %zu byte(s)", sizeof(num));
 printf("\nSize of a long int: %zu byte(s)", sizeof(age));
 printf("\nSize of a long long int: %zu byte(s)", sizeof(nums));
 printf("\nSize of a float: %zu byte(s)", sizeof(doc));
-
 return (0);
 }
