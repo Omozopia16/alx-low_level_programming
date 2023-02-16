5938d979555842f81bb76667982ebf50e144870d
Michael ugenlo <michaelugenlo16@gmail.com>
2023-02-16 15:34:42 -0800

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ long int age;
 long long int nums;
 float doc;
 
-printf("Size of a char : %zu byte(s)", sizeof(number));
-printf("\nSize of an int : %zu byte(s)", sizeof(num));
-printf("\nSize of a long int : %zu byte(s)", sizeof(age));
-printf("\nSize of a long long int : %zu byte(s)", sizeof(nums));
-printf("\nSize of a float : %zu byte(s)", sizeof(doc));
+printf("Size of a char: %zu byte(s)", sizeof(number));
+printf("\nSize of an int: %zu byte(s)", sizeof(num));
+printf("\nSize of a long int: %zu byte(s)", sizeof(age));
+printf("\nSize of a long long int: %zu byte(s)", sizeof(nums));
+printf("\nSize of a float: %zu byte(s)", sizeof(doc));
 
 return (0);
 }
