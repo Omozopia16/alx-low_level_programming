d28b6eee3d3beb51cf46a6d33341fc6d3d70162a
Michael ugenlo <michaelugenlo16@gmail.com>
2023-02-18 00:00:04 -0800

    single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdir.h>
+/**
+ * main -  a program that prints all single digit numbers of base
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar(\n);
+	return (0);
+}
